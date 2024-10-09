aa2193ba7d806260cc2895202ff203603ffa6ad3
Isuu1 <jakub.babelek@gmail.com>
2024-10-09 20:54:13 +0100

    Added CORS to backend

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ const routes = require("./routes");
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3001",
+      "https://asset-insights-technical-test.vercel.app/",
+    ], // Allow requests from this origin
+    methods: ["GET", "POST", "DELETE"], // Allow these methods
+    allowedHeaders: ["Content-Type"], // Allow these headers
+  })
+);
 
 //Connect to MongoDB
 const mongoString = process.env.DATABASE_URL;
